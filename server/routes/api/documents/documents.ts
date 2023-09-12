ae9eed147979a42979cbbdd7b841d684b47ace9d
Apoorv Mishra <apoorvmishra101092@gmail.com>
2023-09-12 15:52:55 +0530

    fix: userId required for preloading collection memberships

--- server/routes/api/documents/documents.ts
+++ server/routes/api/documents/documents.ts
@@ -1380,7 +1380,7 @@ router.post(
     const actor = auth.user;
     const { id, userId, permission } = ctx.input.body;
 
-    const document = await Document.findByPk(id);
+    const document = await Document.findByPk(id, { userId: actor.id });
     authorize(actor, "update", document);
 
     const user = await User.findByPk(userId);
